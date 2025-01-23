4a073e948dc06162af923aa81f5a7f8e69bc1729
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-01-23 20:34:01 +0530

    feat: add updateUser route for user updates

--- src/modules/user/v1/routes/user.route.ts
+++ src/modules/user/v1/routes/user.route.ts
@@ -1,10 +1,15 @@
 import express from 'express';
-import { getUserById, createUser } from '../controllers/user.controller';
+import {
+  getUserById,
+  createUser,
+  updateUser,
+} from '../controllers/user.controller';
 import { validateSchema } from '../../../../core/middlewares/validateSchema';
 import { createUserSchema } from '../schema/user.schema';
 const router = express.Router();
 
 router.post('/', validateSchema(createUserSchema), createUser);
 router.get('/:id', getUserById);
+router.patch('/:id', updateUser);
 
 export default router;

eb1770499375a8267937beb2b9f95999a34b5425
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-01-26 23:14:34 +0530

    feat: add login route and update user patch route to '/me'

--- src/modules/user/v1/routes/user.route.ts
+++ src/modules/user/v1/routes/user.route.ts
@@ -3,13 +3,15 @@ import {
   getUserById,
   createUser,
   updateUser,
+  loginUser,
 } from '../controllers/user.controller';
 import { validateSchema } from '../../../../core/middlewares/validateSchema';
 import { createUserSchema } from '../schema/user.schema';
 const router = express.Router();
 
 router.post('/', validateSchema(createUserSchema), createUser);
+router.post('/login', loginUser);
 router.get('/:id', getUserById);
-router.patch('/:id', updateUser);
+router.patch('/me', updateUser);
 
 export default router;

0f9b8a6e676554a056af3113b254c1741d9b6ae2
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-01-28 12:31:14 +0530

    feat: enhance server logging and add SIGUSR2 signal handling

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import app from './app';
 import prisma from './core/config/prisma';
+import logger from './core/utils/logger';
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,10 +8,10 @@ const startServer = async () => {
   try {
     await prisma.$connect();
     app.listen(PORT, () => {
-      console.log(`app running on port ${PORT}`);
+      logger.info(`app running on port ${PORT}`);
     });
   } catch (err) {
-    console.log(err);
+    logger.error(err);
     process.exit(1);
   }
 };
@@ -19,5 +20,8 @@ process.on('SIGTERM', async () => {
   await prisma.$disconnect();
   process.exit(0);
 });
-
+process.on('SIGUSR2', async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
 startServer();

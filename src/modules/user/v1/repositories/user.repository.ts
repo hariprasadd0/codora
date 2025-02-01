bec230407815dfadc92e943f9a7ce3d34756448f
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-02-01 16:24:36 +0530

    feat: refactor user repository to use a centralized userRepository object

--- src/modules/user/v1/repositories/user.repository.ts
+++ src/modules/user/v1/repositories/user.repository.ts
@@ -1,68 +1,101 @@
 import prisma from '../../../../core/config/prisma';
+import { CreateUserDto, UpdateUserDto } from '../schema/user.schema';
 
-import User from '../types/user';
-export const createUser = async (user: User) => {
-  return await prisma.user.create({
-    data: {
-      email: user.email,
-      name: user.username,
-      passwordHash: user.password,
-    },
-  });
-};
-
-export const logoutUser = async (email: string) => {
-  return await prisma.user.update({
-    where: {
-      email: email,
-    },
-    data: {
-      refreshToken: '',
-    },
-  });
-};
-
-export const getAllUsers = async () => {
-  return await prisma.user.findMany({
-    omit: {
-      passwordHash: true,
-      refreshToken: true,
-    },
-  });
-};
-export const userById = async (userId: number) => {
-  return await prisma.user.findUnique({
-    where: {
-      id: userId,
-    },
-    omit: {
-      passwordHash: true,
-      refreshToken: true,
-    },
-  });
-};
-export const userByEmail = async (email: string) => {
-  return await prisma.user.findUnique({
-    where: {
-      email: email,
-    },
-  });
-};
-export const updateUserById = async (userId: number, user: Partial<User>) => {
-  return await prisma.user.update({
-    where: {
-      id: userId,
-    },
-    data: {
-      ...user,
-    },
-  });
-};
-
-export const deleteUserById = async (userId: number) => {
-  return await prisma.user.delete({
-    where: {
-      id: userId,
-    },
-  });
+export const userRepository = {
+  create: async (user: CreateUserDto) => {
+    return await prisma.user.create({
+      data: {
+        email: user.email,
+        name: user.name,
+        passwordHash: user.password,
+      },
+      omit: {
+        passwordHash: true,
+      },
+    });
+  },
+  getAllUsers: async () => {
+    return await prisma.user.findMany({
+      omit: {
+        passwordHash: true,
+        refreshToken: true,
+      },
+    });
+  },
+  userById: async (userId: number) => {
+    return await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      omit: {
+        passwordHash: true,
+        refreshToken: true,
+      },
+    });
+  },
+  userByEmail: async (email: string) => {
+    return await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+  },
+  updateUserById: async (userId: number, user: Partial<UpdateUserDto>) => {
+    return await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        name: user.name,
+        email: user.email,
+      },
+    });
+  },
+  updateRefreshToken: async (userId: number, refreshToken: string) => {
+    return await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        refreshToken: refreshToken,
+      },
+    });
+  },
+  logoutUser: async (email: string) => {
+    return await prisma.user.update({
+      where: {
+        email: email,
+      },
+      data: {
+        refreshToken: '',
+      },
+    });
+  },
+  deleteUserById: async (userId: number) => {
+    return await prisma.user.delete({
+      where: {
+        id: userId,
+      },
+    });
+  },
+  passwordReset: async (email: string, password: string) => {
+    return await prisma.user.update({
+      where: {
+        email: email,
+      },
+      data: {
+        passwordHash: password,
+      },
+    });
+  },
+  setPreference: async (userId: number, preference: any) => {
+    return await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        preference: preference,
+      },
+    });
+  },
 };

69cdfd712acdc1cb1baaf578d2ab0ef7dcbed51e
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-02-01 16:25:24 +0530

    feat: update user schema to separate create, update, and login validation

--- src/modules/user/v1/schema/user.schema.ts
+++ src/modules/user/v1/schema/user.schema.ts
@@ -1,12 +1,21 @@
 import { z } from 'zod';
 
 export const createUserSchema = z.object({
-  user: z.object({
-    name: z.string().nonempty().trim(),
-    email: z.string().email(),
-    password: z.string().min(6),
-    preference: z.string().optional(),
-  }),
+  name: z.string().nonempty().trim(),
+  email: z.string().email(),
+  password: z.string().min(6),
+  preference: z.string().optional(),
 });
 
-export type CreateUserSchemaType = z.infer<typeof createUserSchema>;
+export const updateUserSchema = z.object({
+  name: z.string().nonempty().trim().optional(),
+  email: z.string().email().optional(),
+  preference: z.string().optional(),
+});
+
+export const loginUserSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+export type CreateUserDto = z.infer<typeof createUserSchema>;
+export type UpdateUserDto = z.infer<typeof updateUserSchema>;

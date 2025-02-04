ee7b5728db28d2fcf6c9ba629692d7893bd0918b
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-02-04 19:41:56 +0530

    feat: implement team repository

--- src/modules/teams/v1/repositories/team.repository.ts
+++ src/modules/teams/v1/repositories/team.repository.ts
@@ -1,17 +1,89 @@
 import prisma from '../../../../core/config/prisma';
+import {
+  AddTeamMemberDto,
+  TeamCreateDto,
+  TeamUpdateDto,
+} from '../schema/team.schema';
 
-export const createTeam = async (team: any) => {
-  return await prisma.team.create({
-    data: {
-      ...team,
-    },
-  });
-};
+export const TeamRepository = {
+  create: async (creatorId: number, team: TeamCreateDto) => {
+    return await prisma.team.create({
+      data: {
+        name: team.name,
+        description: team.description,
+        createdById: creatorId,
+        members: {
+          create: {
+            userId: creatorId,
+            role: 'TEAM_LEAD',
+          },
+        },
+      },
+    });
+  },
+  updateTeam: async (teamId: number, team: TeamUpdateDto) => {
+    return await prisma.team.update({
+      where: {
+        id: teamId,
+      },
+      data: {
+        ...team,
+      },
+    });
+  },
+  getTeamById: async (teamId: number) => {
+    return await prisma.team.findUnique({
+      where: {
+        id: teamId,
+      },
+    });
+  },
+  deleteTeam: async (teamId: number) => {
+    return await prisma.$transaction(async (tx) => {
+      await tx.teamMember.deleteMany({ where: { teamId } });
+      await tx.team.delete({ where: { id: teamId } });
+    });
+  },
+  addMember: async (teamId: number, member: AddTeamMemberDto) => {
+    return await prisma.$transaction(async (tx) => {
+      const existingMember = await tx.teamMember.findUnique({
+        where: {
+          teamId_userId: {
+            teamId,
+            userId: member.userId,
+          },
+        },
+      });
+      if (existingMember) {
+        throw new Error('Member already exists in the team');
+      }
+      const newMember = await tx.teamMember.create({
+        data: {
+          userId: member.userId,
+          teamId,
+          role: member.role,
+        },
+      });
+      return newMember;
+    });
+  },
 
-export const addMember = async (member: any) => {
-  return await prisma.teamMember.create({
-    data: {
-      ...member,
-    },
-  });
+  getMember: async (teamId: number, userId: number) => {
+    return await prisma.teamMember.findFirst({
+      where: {
+        teamId,
+        userId,
+      },
+    });
+  },
+  removeMember: async (teamId: number, userId: number) => {
+    return await prisma.teamMember.delete({
+      where: {
+        teamId_userId: {
+          teamId,
+          userId,
+        },
+      },
+    });
+  },
 };

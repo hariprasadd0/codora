5304ff56e5bc6a6efae9ee25467b7bda6a99d8de
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-02-07 22:16:57 +0530

    feat: add task routes

---
+++ src/modules/tasks/v1/routes/task.route.ts
@@ -0,0 +1,48 @@
+import { Router } from 'express';
+import { validateSchema } from '../../../../core/middlewares/validateSchema';
+import { createTaskSchema } from '../schema/task.schema';
+import { verifyJwt } from '../../../../core/middlewares/auth.middleware';
+import {
+  createTaskController,
+  getTaskByIdController,
+  getAllTasksController,
+  deleteTaskController,
+  assignTaskController,
+  updateTaskController,
+} from '../controllers/task.controllers';
+
+const router = Router();
+
+router.post(
+  '/:projectId',
+  verifyJwt,
+  validateSchema(createTaskSchema),
+  createTaskController
+);
+
+//get single task
+
+router.get('/:id', verifyJwt, getTaskByIdController);
+
+//get all tasks for a project
+router.get('/:projectId', verifyJwt, getAllTasksController);
+
+//assign task to a user
+router.patch('/:id/assign', assignTaskController);
+
+//update status
+router.patch(':id/status');
+//update priority
+router.patch(':id/priority');
+//add dependency
+router.patch(':id/dependency');
+
+//remove dependency
+
+//update tasks
+router.put('/:id', verifyJwt, updateTaskController);
+
+//delete task
+router.delete('/:id', verifyJwt, deleteTaskController);
+
+export default router;

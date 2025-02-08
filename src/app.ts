3fdbeb502db0217cfcfc9216fa41f7988bcdba61
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-02-08 23:52:50 +0530

    feat: implement rate limiting and add task routes to the application

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express, { urlencoded } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import { createServer } from 'http';
 import { Request, Response, NextFunction } from 'express';
-import { initializeServer } from './core/utils/socket';
+import { rateLimit } from 'express-rate-limit';
 import userRoutes from './modules/user/v1/routes/user.route';
 import projectRoutes from './modules/project/v1/routes/project.route';
 import teamRoutes from './modules/teams/v1/routes/team.route';
+import taskRoutes from './modules/tasks/v1/routes/task.route';
 import logger from './core/utils/logger';
 
 const app = express();
@@ -15,10 +15,14 @@ const corsOptions = {
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
 };
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
-const server = createServer(app);
-initializeServer(server);
-
+app.use(limiter);
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
@@ -36,5 +40,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 app.use('/v1/users', userRoutes);
 app.use('/v1/projects', projectRoutes);
 app.use('/v1/teams', teamRoutes);
+app.use('/v1/tasks', taskRoutes);
 
 export default app;

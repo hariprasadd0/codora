3ede2169b6c0fe7c5630e00f4b95eafb3d85dbe9
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-02-08 23:53:07 +0530

    feat: refactor server initialization to use HTTP server and socket integration

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import app from './app';
 import prisma from './core/config/prisma';
 import logger from './core/utils/logger';
+import { createServer } from 'http';
+import { initializeServer } from './core/utils/socket';
 
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
     await prisma.$connect();
-    app.listen(PORT, () => {
+    const server = createServer(app);
+    initializeServer(server);
+    server.listen(PORT, () => {
       logger.info(`app running on port ${PORT}`);
     });
   } catch (err) {

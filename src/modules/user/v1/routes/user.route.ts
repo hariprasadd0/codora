7175311185b82b43e2d6a03c97773461d8508083
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-02-10 23:01:54 +0530

    feat: implement Google authentication routes using Passport.js

--- src/modules/user/v1/routes/user.route.ts
+++ src/modules/user/v1/routes/user.route.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import passport from 'passport';
 import {
   getUserById,
   createUser,
@@ -11,7 +12,43 @@ import {
 import { validateSchema } from '../../../../core/middlewares/validateSchema';
 import { createUserSchema, loginUserSchema } from '../schema/user.schema';
 import { verifyJwt } from '../../../../core/middlewares/auth.middleware';
+import '../../../../core/config/google';
 const router = express.Router();
+
+//google
+router.get('/google', (req, res, next) => {
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+    session: false,
+    failureRedirect: '/login',
+    failureFlash: true,
+  })(req, res, next);
+});
+
+router.get(
+  '/google/redirect',
+  passport.authenticate('google', { session: false }),
+  (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ): void => {
+    if (!req.user) {
+      res.status(401).json({ error: 'Authentication failed' });
+    }
+
+    const { accessToken, refreshToken } = req.user as any;
+
+    // Store refresh token in HttpOnly cookie
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.json({ message: 'Google login successful', accessToken });
+  }
+);
 
 router.post('/register', validateSchema(createUserSchema), createUser);
 router.post('/login', validateSchema(loginUserSchema), loginUser);
@@ -21,4 +58,5 @@ router.patch('/me', verifyJwt, updateUser);
 router.post('/refresh', refreshToken);
 
 router.get('/', getAllUsers);
+
 export default router;

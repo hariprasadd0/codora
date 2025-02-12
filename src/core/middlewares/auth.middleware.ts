8dc928e2c359fdc7cefaca3b6d7407ae4742e4af
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-02-13 01:10:53 +0530

    feat: add middleware to check if user is a team lead

--- src/core/middlewares/auth.middleware.ts
+++ src/core/middlewares/auth.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express';
 import jwt from 'jsonwebtoken';
+import { isUserTeamLead } from '../../modules/teams/v1/services/team.service';
 
 export const verifyJwt: RequestHandler = (
   req: Request,
@@ -30,5 +31,35 @@ export const verifyJwt: RequestHandler = (
       res.status(401).json({ error: 'Unauthorized: Invalid token' });
       return;
     }
+  }
+};
+
+export const isTeamLead = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = (req as any).user; // Extracted from JWT middleware
+    const { teamId } = req.params;
+
+    if (!teamId) {
+      res.status(400).json({ error: 'Team ID is required' });
+    }
+
+    // Check if the user is a team lead
+    const isLead = await isUserTeamLead(user.userId, Number(teamId));
+
+    console.log(isLead);
+
+    if (!isLead) {
+      res.status(403).json({
+        error: 'Access denied. Only Team Leads can perform this action.',
+      });
+    }
+
+    next();
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error.' });
   }
 };

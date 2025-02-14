0ef25d12e4229b67bbbe6be0c613be68e097dd5d
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-02-14 23:51:14 +0530

    feat: implement task assignment with validation and transactional support

--- src/modules/tasks/v1/repositories/task.repository.ts
+++ src/modules/tasks/v1/repositories/task.repository.ts
@@ -1,5 +1,7 @@
 import prisma from '../../../../core/config/prisma';
 import { createTaskDto, updateTaskDto } from '../schema/task.schema';
+import { ApiError } from '../../../../core/utils/ApiError';
+import logger from '../../../../core/utils/logger';
 
 export const TaskRepository = {
   addTask: async (projectId: number, tasks: createTaskDto) => {
@@ -24,6 +26,11 @@ export const TaskRepository = {
   checkAssigned: async (assignedToId: number) => {
     return await prisma.user.findUnique({
       where: { id: assignedToId },
+    });
+  },
+  alreadyAssigned: async (taskId: number, userId: number) => {
+    return await prisma.task.findUnique({
+      where: { id: taskId, assignedToId: userId },
     });
   },
   getTaskById: async (taskId: number) => {
@@ -55,6 +62,56 @@ export const TaskRepository = {
   deleteTask: async (taskId: number) => {
     return await prisma.task.delete({
       where: { id: taskId },
+    });
+  },
+
+  assignTaskTransactional: async (taskId: number, assignedToId: number) => {
+    return await prisma.$transaction(async (tx) => {
+      const [task, user] = await Promise.all([
+        tx.task.findFirst({ where: { id: taskId } }),
+        tx.user.findFirst({ where: { id: assignedToId } }),
+      ]);
+
+      if (!task) {
+        logger.error(`Task not found: ${taskId}`);
+        throw new ApiError(404, 'Task not found');
+      }
+
+      if (!user) {
+        logger.error(`User not found: ${assignedToId}`);
+        throw new ApiError(400, 'Assigned user not found');
+      }
+
+      if (task.assignedToId === assignedToId) {
+        throw new ApiError(400, 'User Already Assigned to the task');
+      }
+
+      const project = await prisma.project.findUnique({
+        where: { id: task.projectId },
+      });
+      if (!project) throw new ApiError(404, 'Project not found');
+      const teamId = project.teamId!;
+
+      if (project.teamId) {
+        const isTeamMember = await prisma.teamMember.findUnique({
+          where: { teamId_userId: { teamId, userId: assignedToId } },
+        });
+        if (!isTeamMember) {
+          throw new ApiError(400, 'User is not a member of this team');
+        }
+      } else {
+        if (assignedToId !== project.createdById) {
+          throw new ApiError(
+            400,
+            'Cannot assign task in solo mode to someone else'
+          );
+        }
+      }
+      // Assign the task
+      return await tx.task.update({
+        where: { id: taskId },
+        data: { assignedToId },
+      });
     });
   },
 };

695e92d38bd47e7fe5a80d4407269ece833ae4a5
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-02-22 21:52:34 +0530

    add crypto

--- src/core/utils/token.ts
+++ src/core/utils/token.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-
+import crypto from 'crypto';
 export const generateAccessToken = (userId: number) => {
   return jwt.sign({ userId }, process.env.JWT_ACCESS_SECRET!, {
     expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
@@ -21,3 +21,5 @@ export const generateResetToken = (userId: number) => {
 export const verifyResetToken = (token: string) => {
   return jwt.verify(token, process.env.JWT_SECRET!) as { userId: number };
 };
+
+export const generateToken = () => crypto.randomBytes(32).toString('hex');

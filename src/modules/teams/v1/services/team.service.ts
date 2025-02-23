dfd66a2d010cae128cefaa0b93c12f274a15cfed
Mr-hariprasad <87761749+Mr-hariprasad@users.noreply.github.com>
2025-02-23 15:37:42 +0530

    feat: enhance member addition with email invitation and token generation

--- src/modules/teams/v1/services/team.service.ts
+++ src/modules/teams/v1/services/team.service.ts
@@ -1,6 +1,8 @@
 import { TeamRepository } from '../repositories/team.repository';
 import { TeamCreateSchema, TeamUpdateSchema } from '../schema/team.schema';
 import { ApiError } from '../../../../core/utils/ApiError';
+import { generateToken } from '../../../../core/utils/token';
+import { sendEmail } from '../../../../core/utils/email';
 export const createTeamService = async (creatorId: number, team: any) => {
   const validated = TeamCreateSchema.parse(team);
   return await TeamRepository.create(creatorId, validated);
@@ -18,21 +20,39 @@ export const updateTeamService = async (teamId: number, team: unknown) => {
 export const deleteTeamService = async (teamId: number) => {
   await TeamRepository.deleteTeam(teamId);
 };
-export const addMemberService = async (teamId: number, member: any) => {
-  const memberExists = await TeamRepository.getMember(teamId, member.userId);
-  if (memberExists) {
-    throw new ApiError(409, 'Member already exists in the team');
+export const addMemberService = async (teamId: number, email: string) => {
+  const user = await TeamRepository.getMemberByEmail(email);
+  if (user) {
+    const memberExists = await TeamRepository.getMember(teamId, user.id);
+    if (memberExists) {
+      throw new ApiError(409, 'Member already exists in the team');
+    } else {
+      const member = await TeamRepository.addMember(teamId, {
+        userId: user.id,
+        role: 'MEMBER',
+      });
+      return member;
+    }
+  } else {
+    const token = generateToken();
+    const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+    const invitation = await TeamRepository.createInvite(
+      teamId,
+      email,
+      token,
+      expiresAt
+    );
+    const inviteLink = `${process.env.FRONTEND_URL}/teams/invite/${token}`;
+    await sendEmail(email, 'Team Invite', inviteLink);
   }
-  const newMember = await TeamRepository.addMember(teamId, member);
-  return newMember;
 };
 
 export const getTeamMembersService = async (teamId: number, userId: number) => {
   await TeamRepository.getMember(teamId, userId);
 };
 
-export const isUserTeamLead = async (teamId: number, userId: number) => {
-  return await TeamRepository.isTeamLead(teamId, userId);
+export const isUserTeamLead = async (userId: number, teamId: number) => {
+  return await TeamRepository.isTeamLead(userId, teamId);
 };
 
 export const removeTeamMemberService = async (
@@ -40,4 +60,8 @@ export const removeTeamMemberService = async (
   userId: number
 ) => {
   await TeamRepository.removeMember(teamId, userId);
+};
+
+export const getInvitationService = async (token: string) => {
+  return await TeamRepository.getInvitation(token);
 };
